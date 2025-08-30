58ee7d47c224f8f41e5e4423c659af7961df0dcd
V-Pavan-Sastry <90331092+V-Pavan-Sastry@users.noreply.github.com>
2025-08-30 08:52:33 +0530

    Update even or odd

--- task0.py
+++ task0.py
@@ -15,3 +15,17 @@ print("After Swapping")
 print(f"Num 1: {num1}")
 print(f"Num 2: {num2}")
 
+######### function to check if a number is even or odd ######
+
+print("\n ########### function to check a number is even or odd######\n")
+def is_even_or_odd(num):
+    if num%2==0:
+        return "Even"
+    else:
+        return "Odd"
+num1=10
+num2=33
+print("Numbers considered for test")
+print(f"Num 1: {num1} is {is_even_or_odd(num1)}")
+print(f"Num 2: {num2} is {is_even_or_odd(num2)}")
+
