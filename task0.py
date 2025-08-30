a5352b6fe85ccebd28d121d867379b75873ccb3a
V-Pavan-Sastry <90331092+V-Pavan-Sastry@users.noreply.github.com>
2025-08-30 08:44:10 +0530

    Update 1st problem

---
+++ task0.py
@@ -0,0 +1,17 @@
+######### swap two numbers without third variable ######
+
+print("\n ########### swap two numbers without third variable ######\n")
+num1=10
+num2=20
+print("Before Swapping")
+print(f"Num 1: {num1}")
+print(f"Num 2: {num2}")
+
+num1 = num1 +num2
+num2 = num1- num2
+num1 = num1 -num2
+
+print("After Swapping")
+print(f"Num 1: {num1}")
+print(f"Num 2: {num2}")
+
